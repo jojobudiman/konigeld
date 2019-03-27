fd3f8e1d847959b2b5083933816399b97c875a16
jovanhidayat <hidayatjovan@gmail.com>
2019-03-27 13:01:01 +0700

    Update send email

--- application/controllers/signup_4.php
+++ application/controllers/signup_4.php
@@ -55,7 +55,44 @@ class signup_4 extends CI_Controller {
         );
         $this->m_crud->insertData($data_outlet, 'outlet_merchant');
         
-        $from_mail = 'noreply@konigeld.com';
+        $this->load->library('email');
+
+        $subject = 'This is a test';
+        $message = '<p>This message has been sent for testing purposes.</p>';
+
+        // Get full html:
+        $body = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=' . strtolower(config_item('charset')) . '" />
+        <title>' . html_escape($subject) . '</title>
+        <style type="text/css">
+        body {
+            font-family: Arial, Verdana, Helvetica, sans-serif;
+            font-size: 16px;
+        }
+        </style>
+        </head>
+        <body>
+        ' . $message . '
+        </body>
+        </html>';
+        // Also, for getting full html you may use the following internal method:
+        //$body = $this->email->full_html($subject, $message);
+
+        $result = $this->email
+            ->from('konigeld@gmail.com')
+            //->reply_to('yoursecondemail@somedomain.com')    // Optional, an account where a human being reads.
+            ->to($this->session->userdata('email'))
+            ->subject($subject)
+            ->message($body)
+            ->send();
+
+        var_dump($result);
+        echo '<br />';
+        echo $this->email->print_debugger();
+        
+        /*$from_mail = 'noreply@konigeld.com';
         $to = $this->session->userdata('email');
         $subject = 'Welcome to Konigeld';
         $message = 'hai';
@@ -70,7 +107,6 @@ class signup_4 extends CI_Controller {
         
         if( $sendtomail ) echo 'Success';
         else echo 'Failed';
-        /*
  
         $headers  = 'MIME-Version: 1.0\r\n';
         
