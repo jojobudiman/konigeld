9b01b2c12d5dcbf2c56a343d51ee9c2103e73df3
jovanhidayat <hidayatjovan@gmail.com>
2019-04-08 19:56:36 +0700

    Update konigeld.sql

--- konigeld.sql
+++ konigeld.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 20, 2019 at 01:52 PM
+-- Generation Time: Apr 08, 2019 at 02:56 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.10
 
@@ -39,6 +39,28 @@ CREATE TABLE `admin` (
   `alamat_admin` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `fname_admin`, `lname_admin`, `email_admin`, `pass_admin`, `status_admin`, `hp_admin`, `alamat_admin`) VALUES
+(1, 'Jovan', 'Hidayat', 'hidayatjovan@gmail.com', 'b59c6e9b344bae1a36fe427a42889265', 1, '081212841621', 'Citra Raya');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id_contact` int(11) NOT NULL,
+  `id_merchant` int(11) NOT NULL,
+  `email` text NOT NULL,
+  `isi` text NOT NULL,
+  `date_contact` text NOT NULL,
+  `status_contact` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- --------------------------------------------------------
 
 --
@@ -64,6 +86,13 @@ CREATE TABLE `detail_transaksi` (
   `tanggal_akhir` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `detail_transaksi`
+--
+
+INSERT INTO `detail_transaksi` (`id_transaksi`, `id_kategori`, `jangka_periode`, `tanggal_awal`, `tanggal_akhir`) VALUES
+(1, 2, '12 month', '2019-04-07', '2020-04-07');
+
 -- --------------------------------------------------------
 
 --
@@ -77,15 +106,6 @@ CREATE TABLE `diskon` (
   `jumlah` double NOT NULL,
   `status_diskon` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `diskon`
---
-
-INSERT INTO `diskon` (`id_diskon`, `id_merchant`, `nama_diskon`, `jumlah`, `status_diskon`) VALUES
-(1, 1, 'Diskon Pemilu', 20000, 1),
-(2, 1, 'Lebaran', 0.3, 1),
-(3, 1, 'HUT RI', 20000, 1);
 
 -- --------------------------------------------------------
 
@@ -128,14 +148,6 @@ CREATE TABLE `jenis_produk` (
   `status_jenis` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `jenis_produk`
---
-
-INSERT INTO `jenis_produk` (`id_jenis_p`, `id_merchant`, `nama_jenis`, `status_jenis`) VALUES
-(1, 1, 'Drinks', 1),
-(2, 1, 'Foods', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -146,10 +158,18 @@ CREATE TABLE `kategori` (
   `id_kategori` int(11) NOT NULL,
   `nama_kategori` text NOT NULL,
   `batas_cabang` int(11) NOT NULL,
-  `batas_user` int(11) NOT NULL,
   `harga` int(11) NOT NULL,
   `status_kategori` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `batas_cabang`, `harga`, `status_kategori`) VALUES
+(1, 'Entry', 1, 25000, 1),
+(2, 'Junior', 5, 75000, 1),
+(3, 'Senior', 999999, 150000, 1);
 
 -- --------------------------------------------------------
 
@@ -164,22 +184,6 @@ CREATE TABLE `langganan` (
   `desain_bon` text NOT NULL,
   `jenis_produk` text NOT NULL,
   `status_user` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `member`
---
-
-CREATE TABLE `member` (
-  `id_member` int(11) NOT NULL,
-  `fname_member` text NOT NULL,
-  `lname_member` text NOT NULL,
-  `hp_member` text NOT NULL,
-  `email_member` text NOT NULL,
-  `poin_member` int(11) NOT NULL,
-  `status_member` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -208,7 +212,7 @@ CREATE TABLE `merchant` (
 --
 
 INSERT INTO `merchant` (`id_merchant`, `fname_merchant`, `lname_merchant`, `nama_bisnis`, `tahun_mulai_bisnis`, `tipe_bisnis`, `monthly_revenue`, `lokasi_bisnis`, `hp_merchant`, `email_merchant`, `pass_merchant`, `status_merchant`) VALUES
-(1, 'Joshua', 'Luih', 'Bisnis Joshua', '2013', 'services', '500_750', 'JKT', '08123456789', 'luih@gmail.com', 'f6bc9a7a6a38809106c931490482e5c4', 1);
+(1, 'Jovan', 'Hidayat', 'Martabak Enak', '2014', 'restaurant', '1500_3000', 'JKT', '081212841621', 'hidayatjovan@gmail.com', 'af288a5c008c7fc65f215853051e6f18', 1);
 
 -- --------------------------------------------------------
 
@@ -266,6 +270,7 @@ CREATE TABLE `outlet_merchant` (
   `id_merchant` int(11) NOT NULL,
   `alamat_outlet` text NOT NULL,
   `hp_outlet` text NOT NULL,
+  `kota` text NOT NULL,
   `provinsi_outlet` text NOT NULL,
   `kodepos_outlet` text NOT NULL,
   `status_outlet` int(11) NOT NULL
@@ -275,8 +280,8 @@ CREATE TABLE `outlet_merchant` (
 -- Dumping data for table `outlet_merchant`
 --
 
-INSERT INTO `outlet_merchant` (`id_outlet`, `id_merchant`, `alamat_outlet`, `hp_outlet`, `provinsi_outlet`, `kodepos_outlet`, `status_outlet`) VALUES
-(1, 1, 'Jl. Jakarta Raya, Jakarta Utara', '08123456789', 'JK', '14045', 1);
+INSERT INTO `outlet_merchant` (`id_outlet`, `id_merchant`, `alamat_outlet`, `hp_outlet`, `kota`, `provinsi_outlet`, `kodepos_outlet`, `status_outlet`) VALUES
+(1, 1, 'Jl. Gunung Sahari Ancol, Jakarta Utara', '081212841621', 'Jakarta Utara', 'DKI Jakarta', '15150', 1);
 
 -- --------------------------------------------------------
 
@@ -294,14 +299,6 @@ CREATE TABLE `produk` (
   `status_produk` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `produk`
---
-
-INSERT INTO `produk` (`id_produk`, `id_outlet`, `id_jenis_p`, `nama_produk`, `harga`, `stok`, `status_produk`) VALUES
-(1, 1, 2, 'Bakso Goreng', '6000', 100, 1),
-(2, 1, 1, 'Lemon Tea', '7000', 10, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -317,6 +314,13 @@ CREATE TABLE `transaksi` (
   `tipe_transaksi` text NOT NULL,
   `status_transaksi` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_admin`, `id_merchant`, `tgl_transaksi`, `total_transaksi`, `tipe_transaksi`, `status_transaksi`) VALUES
+(1, 0, 1, '2019-04-07', '720000', 'Transfer', 1);
 
 -- --------------------------------------------------------
 
@@ -338,13 +342,6 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id_user`, `fname_user`, `lname_user`, `email_user`, `pass_user`, `jabatan`, `hp_user`, `id_outlet`, `id_merchant`, `status_user`) VALUES
-(1, 'Eugenius', 'Pranoto', 'eugeniuspranoto98@gmail.com', '9741da9f7cdd416b0c4b63811770bd6e', 'Janitor', '08123131231', 1, 1, 1);
-
---
 -- Indexes for dumped tables
 --
 
@@ -353,6 +350,12 @@ INSERT INTO `user` (`id_user`, `fname_user`, `lname_user`, `email_user`, `pass_u
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id_contact`);
 
 --
 -- Indexes for table `diskon`
@@ -389,12 +392,6 @@ ALTER TABLE `kategori`
 --
 ALTER TABLE `langganan`
   ADD PRIMARY KEY (`id_langganan`);
-
---
--- Indexes for table `member`
---
-ALTER TABLE `member`
-  ADD PRIMARY KEY (`id_member`);
 
 --
 -- Indexes for table `merchant`
@@ -446,13 +443,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id_contact` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `diskon`
 --
 ALTER TABLE `diskon`
-  MODIFY `id_diskon` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_diskon` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `history`
@@ -470,25 +473,19 @@ ALTER TABLE `history_transaksi`
 -- AUTO_INCREMENT for table `jenis_produk`
 --
 ALTER TABLE `jenis_produk`
-  MODIFY `id_jenis_p` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_jenis_p` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `langganan`
 --
 ALTER TABLE `langganan`
   MODIFY `id_langganan` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `member`
---
-ALTER TABLE `member`
-  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `merchant`
@@ -518,19 +515,19 @@ ALTER TABLE `outlet_merchant`
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
